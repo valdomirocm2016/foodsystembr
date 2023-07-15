58bd1419cc2f924fdf1c9eb31c67ccb2953c2bf7
Valdomiro <valdomirocm@gmail.com>
2023-07-15 11:53:06 -0300

    Configuracao iniciais do projeto

---
+++ src/main/java/com/foodsystemsbr/app/FoodSystemBrApplication.java
@@ -0,0 +1,13 @@
+package com.foodsystemsbr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSystemBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSystemBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodsystemsbr/app/FoodSystemBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodsystemsbr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSystemBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
